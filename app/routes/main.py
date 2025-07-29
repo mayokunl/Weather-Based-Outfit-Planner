44af4a3e46d1d67de60e28b5c3adad220554b525
faith winford <fwinford12@gmail.com>
2025-07-29 11:47:41 -0400

    routing

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from flask_login import login_required, current_user
+from app import db
 
 main_bp = Blueprint('main', __name__)
 
